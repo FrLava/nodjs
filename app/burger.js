4501c847ef2b81df922507b5242a5139a12f90aa
Franc LAVAUD <franc.lavaud@gmail.com>
2017-05-05 17:33:00 +0200

    commit js 2

--- app/burger.js
+++ app/burger.js
@@ -1,54 +1,73 @@
-import { get } from './http.js';
+import { get, post } from './http';
 
-let tabToppingsCache;
-
-function getToppings(promise, type){
-    return promise.then(toppings=>{
-        tabToppingsCache=toppings;
-        console.log(type);
-        return tabToppingsCache;
-    }).then(toppings => toppings.map( t => t.name ))
-        .then(toppings => {
-            let nbToppings=Math.floor(Math.random()*4)+3;
-            let tabRecipe=[];
-
-            for(let i=0;i<nbToppings;i++){
-                let indiceTopping=Math.floor(Math.random()*toppings.length-1)+1;
-
-                if(checkDoublon(toppings[indiceTopping],tabRecipe)){
-                    tabRecipe.push(toppings[indiceTopping]);
-                }else{
-                    i--;
-                }
-            }
-            return tabRecipe;
-        });
+export function getRandomRecipe(options = {}) {
+    return getToppings()
+    .then(filterToppings(options))
+    .then(toStringToppings)
+    .then(doubleToppings)  
+    .then(buildRandomRecipe);
 }
 
+export function saveRecipe(recipe) {
+    console.log('coucou',recipe);
+    return post('http://localhost:3000/recipes', recipe);
+}
 
-
-export function getRandomRecipe(type){
-    
-    if(!tabToppingsCache){
-        return getToppings(get('http://10.1.0.148:3000/toppings'), type);
-        
-    }
-    else{
-        return getToppings(Promise.resolve(tabToppingsCache), type);
+function filterToppings(options) {
+    return function(toppings) {
+        return toppings.filter(topping => 
+            Object.keys(options)
+            .filter(opt => options[opt])
+            .every(opt => topping[opt])
+         )
     }
 }
 
-//compare topping to tab[i]; return false si topping est en doublon
-function checkDoublon(topping,tab){
-    let a=0;
-    for(let i=0;i<tab.length;i++){
-        if(tab[i]===topping){
-            a++;
-        }
+// en entrée : [{ id: number, name: string, vegan: true }, 
+//    { id: number, name: string, vegan: false }]
+// en sortie : [{ id: number, name: string, vegan: true }]
+function filterVeganToppings(toppings) {
+    return toppings.filter(t => t.vegan);
+}
+
+// en entrée : [{ id: number, name: string }]
+// en sortie : [name]
+function toStringToppings(toppings) {
+    return toppings.map(topping => topping.name);
+}
+
+
+// en entrée : [topping]
+// en sortie : [topping, topping]
+function doubleToppings(toppings) {
+    return [...toppings, ...toppings];
+}
+
+let toppingsCache;
+function getToppings() {
+    return toppingsCache
+        ? Promise.resolve(toppingsCache)
+        : get('http://localhost:3000/toppings')
+            .then(toppings => {
+                toppingsCache = toppings;
+                return toppingsCache;
+            });
+}
+
+// en entrée [topping, ...]
+// en sortie recette ([topping1, topping2, ...])
+function getRandomNbToppings(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+function buildRandomRecipe(doubleToppings) {
+    let nbToppings = getRandomNbToppings(3, 6);
+    let recipe = [];
+
+    for (let i = 0; i < nbToppings; i++) {
+        let j = Math.floor(Math.random() * doubleToppings.length);
+        let randomTopping = doubleToppings.splice(j, 1);
+        recipe.push(randomTopping[0]); 
     }
-    if(a>1){
-        return false;
-    }else{
-        return true;
-    }
+    return recipe;
 }
--- app/index.js
+++ app/index.js
@@ -1,8 +1,4 @@
-import { version } from './module.js';
-import { getRandomRecipe } from './burger.js';
-import { get } from './http.js';
-
-
+import { getRandomRecipe,saveRecipe } from './burger.js';
 
 tabBurger: [];
 let ulRecipe=document.createElement('ul');
@@ -16,31 +12,46 @@ bouttonBurgerVegan.name="vegan";
 bouttonBurgerVegan.type="button";
 bouttonBurgerVegan.value="Burger Vegan";
 
+let ButtonAddRecipe=document.createElement('input');
+ButtonAddRecipe.name="addRecipe";
+ButtonAddRecipe.type="button";
+ButtonAddRecipe.value="Ajouter recette";
+
+let normieRecipe = displayRecipeFactory();
+let veganRecipe = displayRecipeFactory({ vegan: true });
 
 var tabToppings=[];
 
-console.log(tabToppings);
+let recipeToAdd;
 
-function showListe(){
-    let type=this.name;
-    ulRecipe.innerHTML = "";
-    getRandomRecipe(type)
-        .then(recipe => {
-            recipe.forEach(toppings => {
-                let li = document.createElement('li');
-                let a = document.createElement('a');
-                a.href="#";
-                a.innerText = toppings;
-                li.appendChild(a)
-                ulRecipe.appendChild(li);
-            });
-            document.body.appendChild(ulRecipe)
-    })
+function displayRecipeFactory(option){
+    return function (){
+        let type=this.name;
+        ulRecipe.innerHTML = "";
+        getRandomRecipe(option)
+            .then(recipe => {
+                recipe.forEach(toppings => {
+                    let li = document.createElement('li');
+                    let a = document.createElement('a');
+                    a.href="#";
+                    a.innerText = toppings;
+                    li.appendChild(a)
+                    ulRecipe.appendChild(li);
+                });
+                document.body.appendChild(ulRecipe);
+                recipeToAdd = recipe;
+                console.log(recipeToAdd);
+        })
+    }
 }
 
 document.body.appendChild(bouttonBurger);
 document.body.appendChild(bouttonBurgerVegan);
+document.body.appendChild(ButtonAddRecipe);
 
 //Listener
-bouttonBurger.addEventListener('click',showListe,false);
-bouttonBurgerVegan.addEventListener('click',showListe,false);
+bouttonBurger.addEventListener('click',normieRecipe,false);
+bouttonBurgerVegan.addEventListener('click',veganRecipe,false);
+ButtonAddRecipe.addEventListener('click',function(){
+     saveRecipe(recipeToAdd);
+    },false);
