6b720e13dacc67616e1c22d0eda1c663aa6323f6
Franc LAVAUD <franc.lavaud@gmail.com>
2017-05-09 17:31:01 +0200

    commit jeu presque ok

--- app/burger.js
+++ app/burger.js
@@ -13,6 +13,53 @@ export function saveRecipe(recipe) {
     return post('http://localhost:3000/recipes', recipe);
 }
 
+//En entrée : [topping: string], [topping: recipe]
+//En sortie: string "valid","invalid","running"
+export function checkBurger(burger,recipe){
+    let flag;
+    for(let i=0;i<burger.length;i++){
+        if(burger[i]===recipe[i]){
+            flag="running";
+
+            if(burger.lenght===recipe.lenght){
+                return flag="valid";
+            }
+        }
+
+        else{
+            return flag="invalid";
+        }
+    }
+
+    return flag;
+
+}
+
+//En entrée : topping, [Tableau de Topping]
+//En sortie : string "valid","invalid","running"
+export function checkBurger2(topping, recipe){
+    let flag;
+
+    //console.log(recipe.tab);
+    //console.log(topping);
+
+    if( (topping===recipe.tab[0]) && (recipe.tab.length>=1) ){
+        recipe.tab.shift;
+        //console.log("ok pas fini");
+        //console.log(recipe.tab);
+        if(recipe.tab.lenght===0){
+            //console.log("fini");
+            return "valid";
+        }
+        return "running";
+    }
+    else if(topping!==recipe.tab[0]){
+        //console.log("pas ok");
+        return "invalid";
+    }
+
+}
+
 function filterToppings(options) {
     return function(toppings) {
         return toppings.filter(topping => 
@@ -44,7 +91,7 @@ function doubleToppings(toppings) {
 }
 
 let toppingsCache;
-function getToppings() {
+export function getToppings() {
     return toppingsCache
         ? Promise.resolve(toppingsCache)
         : get('http://localhost:3000/toppings')
--- app/index.js
+++ app/index.js
@@ -1,57 +1,159 @@
-import { getRandomRecipe,saveRecipe } from './burger.js';
+import { getRandomRecipe,saveRecipe,getToppings,checkBurger2 } from './burger.js';
 
-tabBurger: [];
-let ulRecipe=document.createElement('ul');
-let bouttonBurger=document.createElement('input');
-bouttonBurger.name="normie";
-bouttonBurger.type="button";
-bouttonBurger.value="Burger";
+let inter;
+let randomRecipeList = document.querySelector('.recipe');
+let toppingsList = document.querySelector('.toppings');
+let buttonStop=document.getElementById('stop');
+let buttonStart=document.getElementById('start');
+let listOkToppingsAffichage=document.getElementById('toppingOK');
+let listOkToppings={
+    tab:[]
+};
+let gameEnCours=false;
 
-let bouttonBurgerVegan=document.createElement('input');
-bouttonBurgerVegan.name="vegan";
-bouttonBurgerVegan.type="button";
-bouttonBurgerVegan.value="Burger Vegan";
+//Recupère la liste des ingredients
+let listToppings=getToppings()
+    .then(toppings=>
+        toppings.map(t=>
+            `<button class="btn">${t.name}</button>`
+        
+        ))
+    .then(buttons=>buttons.join(''))
+    .then(buttons=>toppingsList.innerHTML+=buttons);
+    
+toppingsList.addEventListener('click', evt => {
+    if (evt.target.tagName !== 'BUTTON') return;
 
-let ButtonAddRecipe=document.createElement('input');
-ButtonAddRecipe.name="addRecipe";
-ButtonAddRecipe.type="button";
-ButtonAddRecipe.value="Ajouter recette";
-
-let normieRecipe = displayRecipeFactory();
-let veganRecipe = displayRecipeFactory({ vegan: true });
-
-var tabToppings=[];
-
-let recipeToAdd;
-
-function displayRecipeFactory(option){
-    return function (){
-        let type=this.name;
-        ulRecipe.innerHTML = "";
-        getRandomRecipe(option)
-            .then(recipe => {
-                recipe.forEach(toppings => {
-                    let li = document.createElement('li');
-                    let a = document.createElement('a');
-                    a.href="#";
-                    a.innerText = toppings;
-                    li.appendChild(a)
-                    ulRecipe.appendChild(li);
-                });
-                document.body.appendChild(ulRecipe);
-                recipeToAdd = recipe;
-                console.log(recipeToAdd);
-        })
+    if(!gameEnCours){
+        listOkToppings.tab=getListRandomToppings();
+        gameEnCours=true;
     }
+    
+    
+    let res = checkBurger2(evt.target.innerText,listOkToppings);
+
+    if(res==="running"){
+        console.log("running");
+    }
+
+    else if(res==="valid"){
+        gameEnCours=false;
+        console.log("valid");
+    }
+
+    else if(res==="invalid"){
+        console.log("invalid");
+    }
+    
+}, false);
+
+//Génère des ingredients aléatoires
+function randomRecipe(){
+    return function(){
+        gameEnCours=false;
+        randomRecipeList.innerHTML="";
+        getRandomRecipe({ vegan: true })
+        .then(topping=>
+            topping.map(t=>
+                `<li class="liTopping">${t}</li>`
+            
+        ))
+        .then(liens=>liens.join(''))
+        .then(liens=>randomRecipeList.innerHTML+=liens);
+        
+        
+        //console.log(listOkToppings);
+    }
+
 }
 
-document.body.appendChild(bouttonBurger);
-document.body.appendChild(bouttonBurgerVegan);
-document.body.appendChild(ButtonAddRecipe);
 
-//Listener
-bouttonBurger.addEventListener('click',normieRecipe,false);
-bouttonBurgerVegan.addEventListener('click',veganRecipe,false);
-ButtonAddRecipe.addEventListener('click',function(){
-     saveRecipe(recipeToAdd);
-    },false);
+//Bouttons start et stop
+
+buttonStop.addEventListener('click',function(){
+    buttonStart.style='visibility:visible';
+    this.style='visibility:hidden';
+    clearInterval(inter);
+},false);
+
+
+buttonStart.addEventListener('click',function(){
+        buttonStop.style='visibility:visible';
+        this.style='visibility:hidden';
+        randomRecipe()();
+        inter=setInterval(randomRecipe(),30000);
+    }
+,false);
+
+function getListRandomToppings(){
+    let list=[];
+     if(randomRecipeList.hasChildNodes()){
+            for(let i=0;i<randomRecipeList.childNodes.length;i++){
+                list.push(randomRecipeList.childNodes[i].innerText);
+            }
+        }
+    return list;
+}
+
+// let ulRecipe=document.createElement('ul');
+// let bouttonBurger=document.createElement('input');
+// bouttonBurger.name="normie";
+// bouttonBurger.type="button";
+// bouttonBurger.value="Burger";
+
+// let bouttonBurgerVegan=document.createElement('input');
+// bouttonBurgerVegan.name="vegan";
+// bouttonBurgerVegan.type="button";
+// bouttonBurgerVegan.value="Burger Vegan";
+
+// let ButtonAddRecipe=document.createElement('input');
+// ButtonAddRecipe.name="addRecipe";
+// ButtonAddRecipe.type="button";
+// ButtonAddRecipe.value="Ajouter recette";
+
+// let normieRecipe = displayRecipeFactory();
+// let veganRecipe = displayRecipeFactory({ vegan: true });
+
+// var tabToppings=[];
+
+// let recipeToAdd;
+
+// function displayRecipeFactory(option){
+//     return function (){
+//         ulRecipe.innerHTML = "";
+//         getRandomRecipe(option)
+//             .then(recipe => {
+//                 recipe.forEach(toppings => {
+//                     let li = document.createElement('li');
+//                     let a = document.createElement('a');
+//                     a.href="#";
+//                     a.innerText = toppings;
+//                     li.appendChild(a)
+//                     ulRecipe.appendChild(li);
+//                 });
+//                 document.body.appendChild(ulRecipe);
+//                 recipeToAdd = recipe;
+//                 console.log(recipeToAdd);
+//         })
+//     }
+// }
+
+// document.body.appendChild(bouttonBurger);
+// document.body.appendChild(bouttonBurgerVegan);
+// document.body.appendChild(ButtonAddRecipe);
+
+// //let timerNormie=setInterval(normieRecipe,3000);
+// //let timerVegan=setInterval(veganRecipe,3000);
+
+// //Listener
+// bouttonBurger.addEventListener('click',function(){
+//     setInterval(normieRecipe,3000);
+//     },false);
+
+// bouttonBurgerVegan.addEventListener('click',function(){
+//     setInterval(veganRecipe,3000);
+//     },false);
+
+// ButtonAddRecipe.addEventListener('click',function(){
+//      saveRecipe(recipeToAdd);
+//     },false);
